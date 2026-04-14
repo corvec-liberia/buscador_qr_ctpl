3d200af7e2bfe6a8257f9da7cd77d0d69ce6f2e4
corvec-liberia <odcr666@gmail.com>
2026-04-14 15:07:10 -0600

    Add files via upload

---
+++ attendees.js
@@ -0,0 +1,426 @@
+const ATTENDEES = [
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-001",
+    "payload_qr": "ALIM-CORVEC-2026-001",
+    "nombre": "Ana Graciela Alarcón Castillo",
+    "centro_educativo": "CTP Barrio Irvin",
+    "puesto": "Coordinador con la Empresa",
+    "archivo": "ALIM-CORVEC-2026-001_Ana_Graciela_Alarcon_Castillo.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-002",
+    "payload_qr": "ALIM-CORVEC-2026-002",
+    "nombre": "Viviana Loaiciga Corea",
+    "centro_educativo": "CTP Barrio Irvin",
+    "puesto": "Coordinador Académico",
+    "archivo": "ALIM-CORVEC-2026-002_Viviana_Loaiciga_Corea.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-003",
+    "payload_qr": "ALIM-CORVEC-2026-003",
+    "nombre": "Hugo Collado Romero",
+    "centro_educativo": "CTP Barrio Irvin",
+    "puesto": "Coordinador Técnico",
+    "archivo": "ALIM-CORVEC-2026-003_Hugo_Collado_Romero.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-004",
+    "payload_qr": "ALIM-CORVEC-2026-004",
+    "nombre": "Carla Morales Zúñiga",
+    "centro_educativo": "CTP Upala",
+    "puesto": "Coordinador con la Empresa",
+    "archivo": "ALIM-CORVEC-2026-004_Carla_Morales_Zuniga.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-005",
+    "payload_qr": "ALIM-CORVEC-2026-005",
+    "nombre": "Roy Arias Salazar",
+    "centro_educativo": "CTP Upala",
+    "puesto": "Coordinador Técnico",
+    "archivo": "ALIM-CORVEC-2026-005_Roy_Arias_Salazar.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-006",
+    "payload_qr": "ALIM-CORVEC-2026-006",
+    "nombre": "Paula Zamora Chacón",
+    "centro_educativo": "CTP Cañas",
+    "puesto": "Coordinador con la Empresa",
+    "archivo": "ALIM-CORVEC-2026-006_Paula_Zamora_Chacon.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-007",
+    "payload_qr": "ALIM-CORVEC-2026-007",
+    "nombre": "Roberto Pineda Miranda",
+    "centro_educativo": "CINDEA de Tilarán",
+    "puesto": "Coordinador con la Empresa",
+    "archivo": "ALIM-CORVEC-2026-007_Roberto_Pineda_Miranda.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-008",
+    "payload_qr": "ALIM-CORVEC-2026-008",
+    "nombre": "Isabel Oquendo Matarrita",
+    "centro_educativo": "CINDEA de Tilarán",
+    "puesto": "Orientador",
+    "archivo": "ALIM-CORVEC-2026-008_Isabel_Oquendo_Matarrita.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-009",
+    "payload_qr": "ALIM-CORVEC-2026-009",
+    "nombre": "Nurya Vásquez García",
+    "centro_educativo": "CINDEA La Palma",
+    "puesto": "Coordinador con la Empresa",
+    "archivo": "ALIM-CORVEC-2026-009_Nurya_Vasquez_Garcia.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-010",
+    "payload_qr": "ALIM-CORVEC-2026-010",
+    "nombre": "Keylin Salazar Chaverri",
+    "centro_educativo": "CTP Abangares",
+    "puesto": "Coordinador con la Empresa",
+    "archivo": "ALIM-CORVEC-2026-010_Keylin_Salazar_Chaverri.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-011",
+    "payload_qr": "ALIM-CORVEC-2026-011",
+    "nombre": "ARELYS SOTO JIMENEZ",
+    "centro_educativo": "CINDEA de Tilarán",
+    "puesto": "Coordinador Académico",
+    "archivo": "ALIM-CORVEC-2026-011_ARELYS_SOTO_JIMENEZ.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-012",
+    "payload_qr": "ALIM-CORVEC-2026-012",
+    "nombre": "Kattia Mesén Lizano",
+    "centro_educativo": "CTP Upala",
+    "puesto": "Coordinador con la Empresa",
+    "archivo": "ALIM-CORVEC-2026-012_Kattia_Mesen_Lizano.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-013",
+    "payload_qr": "ALIM-CORVEC-2026-013",
+    "nombre": "Marianela Salazar Segnini",
+    "centro_educativo": "CINDEA La Palma",
+    "puesto": "Coordinador Académico",
+    "archivo": "ALIM-CORVEC-2026-013_Marianela_Salazar_Segnini.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-014",
+    "payload_qr": "ALIM-CORVEC-2026-014",
+    "nombre": "ROBERTO RIVERA GARCIA",
+    "centro_educativo": "CINDEA de Tilarán",
+    "puesto": "Coordinador Técnico",
+    "archivo": "ALIM-CORVEC-2026-014_ROBERTO_RIVERA_GARCIA.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-015",
+    "payload_qr": "ALIM-CORVEC-2026-015",
+    "nombre": "GREVEN MIRANDA CORRALES",
+    "centro_educativo": "CINDEA de Tilarán",
+    "puesto": "Director",
+    "archivo": "ALIM-CORVEC-2026-015_GREVEN_MIRANDA_CORRALES.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-016",
+    "payload_qr": "ALIM-CORVEC-2026-016",
+    "nombre": "Olman Fabricio Pineda Rodríguez",
+    "centro_educativo": "CTP Barrio Irvin",
+    "puesto": "Coordinador Técnico",
+    "archivo": "ALIM-CORVEC-2026-016_Olman_Fabricio_Pineda_Rodriguez.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-017",
+    "payload_qr": "ALIM-CORVEC-2026-017",
+    "nombre": "Richard Ignacio Ortiz Miranda",
+    "centro_educativo": "CTP Upala",
+    "puesto": "Coordinador Técnico",
+    "archivo": "ALIM-CORVEC-2026-017_Richard_Ignacio_Ortiz_Miranda.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-018",
+    "payload_qr": "ALIM-CORVEC-2026-018",
+    "nombre": "Jennifer Rodríguez Durán",
+    "centro_educativo": "CINDEA La Palma",
+    "puesto": "Orientador",
+    "archivo": "ALIM-CORVEC-2026-018_Jennifer_Rodriguez_Duran.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-019",
+    "payload_qr": "ALIM-CORVEC-2026-019",
+    "nombre": "Adrian Mata Rodríguez",
+    "centro_educativo": "CINDEA de las Juntas de Abangares",
+    "puesto": "Coordinador Técnico",
+    "archivo": "ALIM-CORVEC-2026-019_Adrian_Mata_Rodriguez.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-020",
+    "payload_qr": "ALIM-CORVEC-2026-020",
+    "nombre": "Ada Iris Cedillos Gutiérrez",
+    "centro_educativo": "CINDEA de las Juntas de Abangares",
+    "puesto": "Coordinador Académico",
+    "archivo": "ALIM-CORVEC-2026-020_Ada_Iris_Cedillos_Gutierrez.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-021",
+    "payload_qr": "ALIM-CORVEC-2026-021",
+    "nombre": "Heilyn Rodríguez Mora",
+    "centro_educativo": "CTP Tronadora",
+    "puesto": "Coordinador con la Empresa",
+    "archivo": "ALIM-CORVEC-2026-021_Heilyn_Rodriguez_Mora.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-022",
+    "payload_qr": "ALIM-CORVEC-2026-022",
+    "nombre": "Beatriz Quirós Elizondo",
+    "centro_educativo": "CTP Tronadora",
+    "puesto": "Coordinador con la Empresa",
+    "archivo": "ALIM-CORVEC-2026-022_Beatriz_Quiros_Elizondo.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-023",
+    "payload_qr": "ALIM-CORVEC-2026-023",
+    "nombre": "Elena Elizondo Murillo",
+    "centro_educativo": "CINDEA de Bebedero",
+    "puesto": "Coordinador Académico",
+    "archivo": "ALIM-CORVEC-2026-023_Elena_Elizondo_Murillo.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-024",
+    "payload_qr": "ALIM-CORVEC-2026-024",
+    "nombre": "Alejandra Cordero Picado",
+    "centro_educativo": "CINDEA de Bebedero",
+    "puesto": "Coordinador Técnico",
+    "archivo": "ALIM-CORVEC-2026-024_Alejandra_Cordero_Picado.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-025",
+    "payload_qr": "ALIM-CORVEC-2026-025",
+    "nombre": "Hilary Cortés Snachezy",
+    "centro_educativo": "CINDEA de Bebedero",
+    "puesto": "Orientador",
+    "archivo": "ALIM-CORVEC-2026-025_Hilary_Cortes_Snachezy.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-026",
+    "payload_qr": "ALIM-CORVEC-2026-026",
+    "nombre": "Gricelda Elizondo Aguilar",
+    "centro_educativo": "CTP Upala",
+    "puesto": "Director",
+    "archivo": "ALIM-CORVEC-2026-026_Gricelda_Elizondo_Aguilar.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-027",
+    "payload_qr": "ALIM-CORVEC-2026-027",
+    "nombre": "Ana García Castillo",
+    "centro_educativo": "CINDEA de Bebedero",
+    "puesto": "Director",
+    "archivo": "ALIM-CORVEC-2026-027_Ana_Garcia_Castillo.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-028",
+    "payload_qr": "ALIM-CORVEC-2026-028",
+    "nombre": "Nilsa Marciaga Corrales",
+    "centro_educativo": "CINDEA de las Juntas de Abangares",
+    "puesto": "Director",
+    "archivo": "ALIM-CORVEC-2026-028_Nilsa_Marciaga_Corrales.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-029",
+    "payload_qr": "ALIM-CORVEC-2026-029",
+    "nombre": "Manuel Durán Pimentel",
+    "centro_educativo": "CTP Tronadora",
+    "puesto": "Director",
+    "archivo": "ALIM-CORVEC-2026-029_Manuel_Duran_Pimentel.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-030",
+    "payload_qr": "ALIM-CORVEC-2026-030",
+    "nombre": "Ana Cecilia Vasquez Rodríguez",
+    "centro_educativo": "CTP Abangares",
+    "puesto": "Coordinador Técnico",
+    "archivo": "ALIM-CORVEC-2026-030_Ana_Cecilia_Vasquez_Rodriguez.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-031",
+    "payload_qr": "ALIM-CORVEC-2026-031",
+    "nombre": "Cindy Johanna Solis Villalobos",
+    "centro_educativo": "CINDEA de las Juntas de Abangares",
+    "puesto": "Coordinador con la Empresa",
+    "archivo": "ALIM-CORVEC-2026-031_Cindy_Johanna_Solis_Villalobos.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-032",
+    "payload_qr": "ALIM-CORVEC-2026-032",
+    "nombre": "Max Valenciano Herrera",
+    "centro_educativo": "CTP Tronadora",
+    "puesto": "Coordinador Técnico",
+    "archivo": "ALIM-CORVEC-2026-032_Max_Valenciano_Herrera.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-033",
+    "payload_qr": "ALIM-CORVEC-2026-033",
+    "nombre": "Sandy Alonso Jiménez Cascante",
+    "centro_educativo": "CTP Abangares",
+    "puesto": "Director",
+    "archivo": "ALIM-CORVEC-2026-033_Sandy_Alonso_Jimenez_Cascante.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-034",
+    "payload_qr": "ALIM-CORVEC-2026-034",
+    "nombre": "Oscar Campos Rodríguez",
+    "centro_educativo": "CTP Liberia",
+    "puesto": "Coordinador con la Empresa",
+    "archivo": "ALIM-CORVEC-2026-034_Oscar_Campos_Rodriguez.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-035",
+    "payload_qr": "ALIM-CORVEC-2026-035",
+    "nombre": "Estephanny Hurtado Rojas",
+    "centro_educativo": "CTP Tronadora",
+    "puesto": "Orientador",
+    "archivo": "ALIM-CORVEC-2026-035_Estephanny_Hurtado_Rojas.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-036",
+    "payload_qr": "ALIM-CORVEC-2026-036",
+    "nombre": "Auxiliadora Marchena Contreras",
+    "centro_educativo": "CTP Liberia",
+    "puesto": "Orientador",
+    "archivo": "ALIM-CORVEC-2026-036_Auxiliadora_Marchena_Contreras.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-037",
+    "payload_qr": "ALIM-CORVEC-2026-037",
+    "nombre": "Enercy Rodríguez Cháves",
+    "centro_educativo": "CTP Upala",
+    "puesto": "Orientador",
+    "archivo": "ALIM-CORVEC-2026-037_Enercy_Rodriguez_Chaves.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-038",
+    "payload_qr": "ALIM-CORVEC-2026-038",
+    "nombre": "Verónica Muñoz Chaves",
+    "centro_educativo": "CTP Cañas",
+    "puesto": "Orientador",
+    "archivo": "ALIM-CORVEC-2026-038_Veronica_Munoz_Chaves.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-039",
+    "payload_qr": "ALIM-CORVEC-2026-039",
+    "nombre": "Gabriela Marchena Morales",
+    "centro_educativo": "CTP Liberia",
+    "puesto": "Coordinador con la Empresa",
+    "archivo": "ALIM-CORVEC-2026-039_Gabriela_Marchena_Morales.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-040",
+    "payload_qr": "ALIM-CORVEC-2026-040",
+    "nombre": "Abel David Mendoza Ortiz",
+    "centro_educativo": "CTP Liberia",
+    "puesto": "Coordinador Técnico",
+    "archivo": "ALIM-CORVEC-2026-040_Abel_David_Mendoza_Ortiz.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-041",
+    "payload_qr": "ALIM-CORVEC-2026-041",
+    "nombre": "Lourdes Davila Romero",
+    "centro_educativo": "CTP Liberia",
+    "puesto": "Coordinador Técnico",
+    "archivo": "ALIM-CORVEC-2026-041_Lourdes_Davila_Romero.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-042",
+    "payload_qr": "ALIM-CORVEC-2026-042",
+    "nombre": "Paola a Ramírez Briceño",
+    "centro_educativo": "CTP Liberia",
+    "puesto": "Director",
+    "archivo": "ALIM-CORVEC-2026-042_Paola_a_Ramirez_Briceno.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-043",
+    "payload_qr": "ALIM-CORVEC-2026-043",
+    "nombre": "Luís Castillo García",
+    "centro_educativo": "CTP Barrio Irvin",
+    "puesto": "Coordinador con la Empresa",
+    "archivo": "ALIM-CORVEC-2026-043_Luis_Castillo_Garcia.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-044",
+    "payload_qr": "ALIM-CORVEC-2026-044",
+    "nombre": "Leiner Castillo Rodríguez",
+    "centro_educativo": "CTP Abangares",
+    "puesto": "Coordinador Técnico",
+    "archivo": "ALIM-CORVEC-2026-044_Leiner_Castillo_Rodriguez.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-045",
+    "payload_qr": "ALIM-CORVEC-2026-045",
+    "nombre": "Ginnette Bustos Mendoza",
+    "centro_educativo": "CTP Abangares",
+    "puesto": "Coordinador Académico",
+    "archivo": "ALIM-CORVEC-2026-045_Ginnette_Bustos_Mendoza.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-046",
+    "payload_qr": "ALIM-CORVEC-2026-046",
+    "nombre": "Ermis Alfredo Aparicio Pérez",
+    "centro_educativo": "CTP Cañas",
+    "puesto": "Coordinador Técnico",
+    "archivo": "ALIM-CORVEC-2026-046_Ermis_Alfredo_Aparicio_Perez.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-047",
+    "payload_qr": "ALIM-CORVEC-2026-047",
+    "nombre": "Erika Mohr Jiménez",
+    "centro_educativo": "CINDEA La Palma",
+    "puesto": "Director",
+    "archivo": "ALIM-CORVEC-2026-047_Erika_Mohr_Jimenez.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-048",
+    "payload_qr": "ALIM-CORVEC-2026-048",
+    "nombre": "Juan Carlos Picado Delgado",
+    "centro_educativo": "CTP Upala",
+    "puesto": "Otro",
+    "archivo": "ALIM-CORVEC-2026-048_Juan_Carlos_Picado_Delgado.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-049",
+    "payload_qr": "ALIM-CORVEC-2026-049",
+    "nombre": "Oscar Mario Jiménez Piña",
+    "centro_educativo": "CTP Cañas",
+    "puesto": "Coordinador Técnico",
+    "archivo": "ALIM-CORVEC-2026-049_Oscar_Mario_Jimenez_Pina.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-050",
+    "payload_qr": "ALIM-CORVEC-2026-050",
+    "nombre": "Gabriela Trujillo Sisfontes",
+    "centro_educativo": "CTP Fortuna de Bagaces",
+    "puesto": "Coordinador Académico",
+    "archivo": "ALIM-CORVEC-2026-050_Gabriela_Trujillo_Sisfontes.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-051",
+    "payload_qr": "ALIM-CORVEC-2026-051",
+    "nombre": "Silvia Elena Ulloa Bogantes",
+    "centro_educativo": "CTP Abangares",
+    "puesto": "Orientador",
+    "archivo": "ALIM-CORVEC-2026-051_Silvia_Elena_Ulloa_Bogantes.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-052",
+    "payload_qr": "ALIM-CORVEC-2026-052",
+    "nombre": "Olivia Garay Gutierrez",
+    "centro_educativo": "CTP Abangares",
+    "puesto": "Coordinador con la Empresa",
+    "archivo": "ALIM-CORVEC-2026-052_Olivia_Garay_Gutierrez.png"
+  },
+  {
+    "codigo_qr": "ALIM-CORVEC-2026-053",
+    "payload_qr": "ALIM-CORVEC-2026-053",
+    "nombre": "Karla Arce Soto",
+    "centro_educativo": "CTP Cañas",
+    "puesto": "Coordinador con la Empresa",
+    "archivo": "ALIM-CORVEC-2026-053_Karla_Arce_Soto.png"
+  }
+];
